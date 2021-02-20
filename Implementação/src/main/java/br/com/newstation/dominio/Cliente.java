588fcfb157d58dd9aaf200a8b3e7b46b7db60fce
BrenoXDMoon <brenogabriel19@gmail.com>
2021-02-20 11:35:36 -0300

    Add CartaoCredito.java

--- Implementação/src/main/java/br/com/newstation/dominio/Cliente.java
+++ Implementação/src/main/java/br/com/newstation/dominio/Cliente.java
@@ -11,43 +11,43 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 @Entity
-public class Cliente extends EntidadeDominio{
+public class Cliente extends EntidadeDominio {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Embedded
 	private TipoCliente tipoCliente;
 	private String nome;
 	private String cpf;
-	
+
 	@OneToMany
 	private List<Endereco> enderecos = new ArrayList<Endereco>();
-	
+
 	@OneToMany
 	private List<CartaoCredito> cartoes;
-	
+
 	public TipoCliente getTipoCliente() {
 		return tipoCliente;
 	}
-	
+
 	public void setTipoCliente(TipoCliente tipoCliente) {
 		this.tipoCliente = tipoCliente;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public List<Endereco> getEnderecos() {
 		return enderecos;
 	}
-	
+
 	public void setEnderecos(List<Endereco> enderecos) {
 		this.enderecos = enderecos;
 	}
