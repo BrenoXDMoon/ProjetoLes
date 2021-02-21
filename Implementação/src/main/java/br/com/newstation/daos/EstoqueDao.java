6d8dfc93245f1194c01329ce94bf0c66e91b5bdf
BrenoXDMoon <brenogabriel19@gmail.com>
2021-02-20 21:23:11 -0300

    Removendo AdminCartaBean

--- Implementação/src/main/java/br/com/newstation/daos/EstoqueDao.java
+++ Implementação/src/main/java/br/com/newstation/daos/EstoqueDao.java
@@ -1,14 +1,21 @@
 package br.com.newstation.daos;
 
+import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.com.newstation.dominio.Estoque;
 
+@Stateful
 public class EstoqueDao {
 
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Deprecated
+	public EstoqueDao() {
+		// TODO Auto-generated constructor stub
+	}
 	
 	public void salvar(Estoque estoque) {
 		manager.persist(estoque);
