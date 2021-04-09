0aff64de78f438e77a7fb2cd8e9895a77fdc7715
shirinou <shirinouhimura@gmail.com>
2021-04-09 18:23:49 -0300

    baguncinha com js

--- Implementação/src/main/webapp/assets/js/cartoes.js
+++ Implementação/src/main/webapp/assets/js/cartoes.js
@@ -8,4 +8,52 @@ function listaCartao(){
 	document.getElementById("listaCartao").classList.toggle("d-none");
 }
 
+
 document.getElementById("botaoCartao").addEventListener("click", listaCartao);
+
+function doisCartoes() {
+	var cartao = document.getElementById("j_idt8:cartao").getElementsByTagName("input");
+	var limit = 2;
+	for (var i = 0; i < cartao.length; i++) {
+		cartao[i].onclick = function() {
+			var checkedcount = 0;
+				for (var i = 0; i < cartao.length; i++) {
+				checkedcount += (cartao[i].checked) ? 1 : 0;
+			}
+			if (checkedcount > limit) {
+				this.checked = false;
+			}
+		}
+	}
+}
+
+function teste(){
+	var total = document.getElementById("totalFrete").innerHTML;
+	var cartao_1 = document.getElementById("j_idt8:valorCartaoUm").value;
+	var cartao_2 = document.getElementById("j_idt8:valorCartaoDois").value;
+	
+	if(total - (cartao_1 + cartao_2) < 10)
+		console.log("dru");
+}
+
+function getCheckedBoxes() {
+  var total = document.getElementById("totalFrete").innerHTML;
+  var checkboxes = document.getElementById("j_idt8:cupomTroca").getElementsByTagName("input");
+  var soma = 0;
+  // loop over them all
+  for (var i=0; i<checkboxes.length; i++) {
+     // And stick the checked ones onto an array...
+     if (checkboxes[i].checked) {
+     		console.log(checkboxes[i].id);
+ 			soma =+ document.getElementById('j_idt8:cupomTroca:0').nextSibling.innerHTML.split(":")[1];
+         
+     }
+  }
+  
+  if(soma > (total*1.2)){
+  	alert("Os Valores dos cupons ultrapassam muito o valor da compra ");
+  }
+  return soma;
+}
+
+
